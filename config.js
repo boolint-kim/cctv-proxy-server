f76e094fd631511846cb784a76f86dea44bd3f24
boolint.kim <boolint.kim@gmail.com>
2026-02-11 17:36:56 +0900

    remove config.js from tracking

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    AUTHORIZED_IP: '20.41.122.77',  // 실제 서버 IP로 업데이트
-    CCTV_API_KEY: 'spdYlAuDpMu815Bqun6bM4xMjg7gBtVChlcFWMEUGqDvbRRDx9OSu8n2gXlrj3',
-    MOBILE_APP_KEY: 'your-mobile-app-secret-key-123',
-    PORT: 3000
-};
